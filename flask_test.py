652cc380d7641bd3c139493a357cd94edaabb4ac
AyushStarGupta <37546890+AyushStarGupta@users.noreply.github.com>
2020-11-14 23:07:08 +0530

    bleh

--- flask_test.py
+++ flask_test.py
@@ -1,23 +1,26 @@
-import numpy as np
 import pandas as pd
 import pickle
-import base64
 from flask import Flask, jsonify
+import os
 
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, 'all_data_final.csv')
 app = Flask("EPP-Predictor")
 # CORS(app, resources={r"/": {"origins": "*"}})
 
 filename = 'finalized_model_xgboost.sav'
 model = pickle.load(open(filename, 'rb'))
 
-all_df = pd.read_csv(r'/Users/Pranjal/Desktop/LatestProjects/EPP-predictor/all_data_final.csv')
+all_df = pd.read_csv(path)
 
-def get_data(db,lat,longi):
-    lat = round(lat*4)/4
-    longi = round(longi*4)/4
+
+def get_data(db, lat, longi):
+    lat = round(lat * 4) / 4
+    longi = round(longi * 4) / 4
     df = db[db["latitude"] == lat]
     df = df[df["longitude"] == longi]
     return df
+
 
 # lat = st.number_input('Enter lat')
 # ns = st.selectbox('', ('N', 'S'))
@@ -30,19 +33,19 @@ def predictor(lat, longi):
     lat = float(lat)
     longi = float(longi)
     print(lat, longi)
-    if -90<=lat<=90 and -180<=longi<=180:
+    if -90 <= lat <= 90 and -180 <= longi <= 180:
 
         longi = 180 + longi
         if longi > 359.75:
             return "Enter valid parameters"
-        
-        print(lat,longi)
-        a = (get_data(all_df,lat,longi))
 
-        a = a.iloc[:,[2,3,4,5,6,7,8]]
+        print(lat, longi)
+        a = (get_data(all_df, lat, longi))
+
+        a = a.iloc[:, [2, 3, 4, 5, 6, 7, 8]]
         prediction = model.predict(a)
-        prediction = float(prediction)*100
-        prediction = round(prediction,3)
+        prediction = float(prediction) * 100
+        prediction = round(prediction, 3)
 
         r = jsonify({"text": f'There is a {prediction}% chance that a solar plant built there will be successful'})
         r.headers.add('Access-Control-Allow-Origin', '*')
